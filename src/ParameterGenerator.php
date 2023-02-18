c5f66996bbe3dfda197f4825525186351ac70f7b
William Knauss <william.knauss@kingdom.com>
2023-02-18 13:11:27 -0500

    more phpinsight updates

--- src/ParameterGenerator.php
+++ src/ParameterGenerator.php
@@ -6,6 +6,7 @@ namespace FightTheIce\Code;
 
 use Laminas\Code\Generator\ParameterGenerator as Laminas_ParameterGenerator;
 
-class ParameterGenerator extends Laminas_ParameterGenerator {
+class ParameterGenerator extends Laminas_ParameterGenerator
+{
     use Traits\TypeHinterTrait;
 }
