c4cf1331ca98c627ddb706bd0ea45f18e906452a
William Knauss <william.knauss@kingdom.com>
2023-02-19 18:31:43 -0500

    u2d

--- src/TypeHintGenerator.php
+++ src/TypeHintGenerator.php
@@ -9,173 +9,171 @@ use Laminas\Code\Generator\TypeGenerator;
 
 class TypeHintGenerator
 {
-	/**
-	 * typeHint
-	 *
-	 * The original type hint
-	 *
-	 * @access protected
-	 * @property string|null $typeHint The original type hint
-	 */
-	protected ?string $typeHint = null;
+    /**
+     * typeHint
+     *
+     * The original type hint
+     *
+     * @access protected
+     *
+     * @property string|null $typeHint The original type hint
+     */
+    protected ?string $typeHint = null;
 
-	/**
-	 * baseHint
-	 *
-	 * The base hint of the type hint (ie. ?string, would be string)
-	 *
-	 * @access protected
-	 * @property string|null $baseHint The base hint of the type hint (ie. ?string,
-	 * would be string)
-	 */
-	protected ?string $baseHint;
+    /**
+     * baseHint
+     *
+     * The base hint of the type hint (ie. ?string, would be string)
+     *
+     * @access protected
+     *
+     * @property string|null $baseHint The base hint of the type hint (ie. ?string,
+     * would be string)
+     */
+    protected ?string $baseHint;
 
-	/**
-	 * nullable
-	 *
-	 * Can this type hint also be null?
-	 *
-	 * @access protected
-	 * @property bool $nullable Can this type hint also be null?
-	 */
-	protected bool $nullable = false;
+    /**
+     * nullable
+     *
+     * Can this type hint also be null?
+     *
+     * @access protected
+     *
+     * @property bool $nullable Can this type hint also be null?
+     */
+    protected bool $nullable = false;
 
-	/**
-	 * inited
-	 *
-	 * Has the constructor been inited?
-	 *
-	 * @access protected
-	 * @property bool $inited Has the constructor been inited?
-	 */
-	protected bool $inited = false;
+    /**
+     * inited
+     *
+     * Has the constructor been inited?
+     *
+     * @access protected
+     *
+     * @property bool $inited Has the constructor been inited?
+     */
+    protected bool $inited = false;
 
+    /**
+     * __construct
+     *
+     * Class construct
+     *
+     * @access public
+     *
+     * @param string|null $type The type hint as a string
+     */
+    public function __construct(?string $type = null)
+    {
+        if ($this->inited === true) {
+            throw new Exception(self::class.'::__construct has already been initialzed!');
+        }
 
-	/**
-	 * __construct
-	 *
-	 * Class construct
-	 *
-	 * @access public
-	 * @param string|null $type The type hint as a string
-	 */
-	public function __construct(?string $type = null)
-	{
-		if ($this->inited === true) {
-		    throw new Exception(self::class.'::__construct has already been initialzed!');
-		}
+        $this->inited = true;
 
-		$this->inited = true;
+        $type = trim($type);
 
-		$type = trim($type);
+        if (is_null($type)) {
+            return;
+        }
 
-		if (is_null($type)) {
-		    return;
-		}
+        $type = trim($type);
+        if (strlen($type) === 0) {
+            throw new Exception(self::class.'::__construct expects parameter $type to be a non empty string!');
+        }
 
-		$type = trim($type);
-		if (strlen($type) === 0) {
-		    throw new Exception(self::class.'::__construct expects parameter $type to be a non empty string!');
-		}
+        $this->typeHint = $type;
+        $this->baseHint = $type;
+        if (substr($type, 0, 1) === '?') {
+            $this->baseHint = substr($type, 1);
+            $this->nullable = true;
+        }
+    }
 
-		$this->typeHint = $type;
-		$this->baseHint = $type;
-		if (substr($type, 0, 1) === '?') {
-		    $this->baseHint = substr($type, 1);
-		    $this->nullable = true;
-		}
-	}
+    /**
+     * hasType
+     *
+     * Used to determine if the provided type hint has a type
+     *
+     * @access public
+     */
+    public function hasType(): bool
+    {
+        return ! is_null($this->typeHint);
+    }
 
+    /**
+     * getTypeHint
+     *
+     * Returns the original typehint or null if none was set
+     *
+     * @access public
+     */
+    public function getTypeHint(): ?string
+    {
+        return $this->typeHint;
+    }
 
-	/**
-	 * hasType
-	 *
-	 * Used to determine if the provided type hint has a type
-	 *
-	 * @access public
-	 */
-	public function hasType(): bool
-	{
-		return ! is_null($this->typeHint);
-	}
+    /**
+     * getBaseType
+     *
+     * Returns the base type hint (i.e. If ?string then this would return string)
+     *
+     * @access public
+     */
+    public function getBaseType(): ?string
+    {
+        return $this->baseHint;
+    }
 
+    /**
+     * isNullable
+     *
+     * Used to determine if this type hint can also be null
+     *
+     * @access public
+     */
+    public function isNullable(): bool
+    {
+        return $this->nullable;
+    }
 
-	/**
-	 * getTypeHint
-	 *
-	 * Returns the original typehint or null if none was set
-	 *
-	 * @access public
-	 */
-	public function getTypeHint(): ?string
-	{
-		return $this->typeHint;
-	}
+    /**
+     * getVarTag
+     *
+     * Returns the datatypes used in this type hint, usefull for docblock
+     *
+     * @access public
+     */
+    public function getVarTag(): string
+    {
+        if ($this->hasType() === false) {
+            return '';
+        }
 
+        $var = $this->getBaseType();
 
-	/**
-	 * getBaseType
-	 *
-	 * Returns the base type hint (i.e. If ?string then this would return string)
-	 *
-	 * @access public
-	 */
-	public function getBaseType(): ?string
-	{
-		return $this->baseHint;
-	}
+        if ($this->isNullable() === true) {
+            $var .= '|null';
+        }
 
+        return $var;
+    }
 
-	/**
-	 * isNullable
-	 *
-	 * Used to determine if this type hint can also be null
-	 *
-	 * @access public
-	 */
-	public function isNullable(): bool
-	{
-		return $this->nullable;
-	}
+    /**
+     * getTypeGenerator
+     *
+     * Returns the Laminas\Code\GeneratorTypeGenerator object or null if one isn't
+     * able to be created
+     *
+     * @access public
+     */
+    public function getTypeGenerator(): ?TypeGenerator
+    {
+        if ($this->hasType() === false) {
+            return null;
+        }
 
-
-	/**
-	 * getVarTag
-	 *
-	 * Returns the datatypes used in this type hint, usefull for docblock
-	 *
-	 * @access public
-	 */
-	public function getVarTag(): string
-	{
-		if ($this->hasType() === false) {
-		    return '';
-		}
-
-		$var = $this->getBaseType();
-
-		if ($this->isNullable() === true) {
-		    $var.= '|null';
-		}
-
-		return $var;
-	}
-
-
-	/**
-	 * getTypeGenerator
-	 *
-	 * Returns the Laminas\Code\GeneratorTypeGenerator object or null if one isn't
-	 * able to be created
-	 *
-	 * @access public
-	 */
-	public function getTypeGenerator(): ?TypeGenerator
-	{
-		if ($this->hasType() === false) {
-		    return null;
-		}
-
-	    return TypeGenerator::fromTypeString($this->typeHint);
-	}
+        return TypeGenerator::fromTypeString($this->typeHint);
+    }
 }
