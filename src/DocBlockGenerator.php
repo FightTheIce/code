e9a460b23fef1df8b58ce9413cd61fd9aab6fd3c
William Knauss <william.knauss@kingdom.com>
2023-02-20 03:10:11 -0500

    phpinsights auto fix

--- src/DocBlockGenerator.php
+++ src/DocBlockGenerator.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace FightTheIce\Code;
 
-use Laminas\Code\Generator\DocBlockGenerator as Laminas_DocBlockGenerator;
 use Laminas\Code\Generator\DocBlock\Tag\AuthorTag;
 use Laminas\Code\Generator\DocBlock\Tag\GenericTag;
 use Laminas\Code\Generator\DocBlock\Tag\LicenseTag;
@@ -14,74 +13,85 @@ use Laminas\Code\Generator\DocBlock\Tag\PropertyTag;
 use Laminas\Code\Generator\DocBlock\Tag\ReturnTag;
 use Laminas\Code\Generator\DocBlock\Tag\ThrowsTag;
 use Laminas\Code\Generator\DocBlock\Tag\VarTag;
+use Laminas\Code\Generator\DocBlockGenerator as Laminas_DocBlockGenerator;
 
-class DocBlockGenerator extends Laminas_DocBlockGenerator {
-    public function newAuthorTag(mixed $authorName = null, mixed $authorEmail = null): AuthorTag {
-        $tag = new AuthorTag($authorName,$authorEmail);
-
-        $this->setTag($tag);
-
-        return $tag;
-    }
-
-    public function newGenericTag(mixed $name = null, mixed $content = null): GenericTag {
-        $tag = new GenericTag($name,$content);
+class DocBlockGenerator extends Laminas_DocBlockGenerator
+{
+    public function newAuthorTag(mixed $authorName = null, mixed $authorEmail = null): AuthorTag
+    {
+        $tag = new AuthorTag($authorName, $authorEmail);
 
         $this->setTag($tag);
 
         return $tag;
     }
 
-    public function newLicenseTag(mixed $url = null, mixed $licenseName = null): LicenseTag {
-        $tag = new LicenseTag($url,$licenseName);
+    public function newGenericTag(mixed $name = null, mixed $content = null): GenericTag
+    {
+        $tag = new GenericTag($name, $content);
 
         $this->setTag($tag);
 
         return $tag;
     }
 
-    public function newMethodTag(mixed $methodName = null, array $types = [], mixed $description = null, bool $isStatic = false): MethodTag {
-        $tag = new MethodTag($methodName,$types,$description,$isStatic);
+    public function newLicenseTag(mixed $url = null, mixed $licenseName = null): LicenseTag
+    {
+        $tag = new LicenseTag($url, $licenseName);
 
         $this->setTag($tag);
 
         return $tag;
     }
 
-    public function newParamTag(mixed $variableName = null, array $types = [], mixed $description = null): ParamTag {
-        $tag = new ParamTag($variableName,$types,$description);
+    public function newMethodTag(mixed $methodName = null, array $types = [], mixed $description = null, bool $isStatic = false): MethodTag
+    {
+        $tag = new MethodTag($methodName, $types, $description, $isStatic);
 
         $this->setTag($tag);
 
         return $tag;
     }
 
-    public function newPropertyTag(mixed $propertyName = null, array $types = [], mixed $description = null): PropertyTag {
-        $tag = new PropertyTag($propertyName,$types,$description);
+    public function newParamTag(mixed $variableName = null, array $types = [], mixed $description = null): ParamTag
+    {
+        $tag = new ParamTag($variableName, $types, $description);
 
         $this->setTag($tag);
 
         return $tag;
     }
 
-    public function newReturnTag(array $types = [], mixed $description = null): ReturnTag {
-        $tag = new ReturnTag($types,$description);
+    public function newPropertyTag(mixed $propertyName = null, array $types = [], mixed $description = null): PropertyTag
+    {
+        $tag = new PropertyTag($propertyName, $types, $description);
 
         $this->setTag($tag);
 
         return $tag;
     }
 
-    public function newThrowsTag(array $types = [], mixed $description = null): ThrowsTag {
-        $tag = new ThrowsTag($types,$description);
+    public function newReturnTag(array $types = [], mixed $description = null): ReturnTag
+    {
+        $tag = new ReturnTag($types, $description);
 
         $this->setTag($tag);
 
         return $tag;
     }
 
-    public function newVarTag(?string $variableName = null, array $types = [], ?string $description = null): VarTag {
-        $tag = new VarTag($variableName,$types,$description);
+    public function newThrowsTag(array $types = [], mixed $description = null): ThrowsTag
+    {
+        $tag = new ThrowsTag($types, $description);
+
+        $this->setTag($tag);
+
+        return $tag;
+    }
+
+    public function newVarTag(?string $variableName = null, array $types = [], ?string $description = null): VarTag
+    {
+        $tag = new VarTag($variableName, $types, $description);
 
         $this->setTag($tag);
 
--- src/MethodGenerator.php
+++ src/MethodGenerator.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace FightTheIce\Code;
 
+use Laminas\Code\Generator\DocBlockGenerator;
 use Laminas\Code\Generator\MethodGenerator as Laminas_MethodGenerator;
 use Laminas\Code\Generator\ParameterGenerator;
 use Laminas\Code\Generator\PromotedParameterGenerator;
-use Laminas\Code\Generator\DocBlockGenerator;
 
-class MethodGenerator extends Laminas_MethodGenerator {
+class MethodGenerator extends Laminas_MethodGenerator
+{
     public function newDocBlockGenerator(mixed $shortDescription = null, mixed $longDescription = null, array $tags = []): DocBlockGenerator
     {
         $docblock = new DocBlockGenerator($shortDescription, $longDescription, $tags);
@@ -19,16 +20,18 @@ class MethodGenerator extends Laminas_MethodGenerator {
         return $docblock;
     }
 
-    public function newParameterGenerator(mixed $name = null, mixed $type = null, mixed $defaultValue = null, mixed $position = null, mixed $passByReference = null): ParameterGenerator {
-        $parameter = new ParameterGenerator($name,$type,$defaultValue,$position,$passByReference);
+    public function newParameterGenerator(mixed $name = null, mixed $type = null, mixed $defaultValue = null, mixed $position = null, mixed $passByReference = null): ParameterGenerator
+    {
+        $parameter = new ParameterGenerator($name, $type, $defaultValue, $position, $passByReference);
 
         $this->setParameter($parameter);
 
         return $parameter;
     }
 
-    public function newPromotedParameterGenerator(string $name, ?string $type = null, string $visibility = PromotedParameterGenerator::VISIBILITY_PUBLIC, ?int $position = null, bool $passByReference = false): PromotedParameterGenerator {
-        $parameter = new PromotedParameterGenerator($name,$type,$visibility,$position,$passByReference);
+    public function newPromotedParameterGenerator(string $name, ?string $type = null, string $visibility = PromotedParameterGenerator::VISIBILITY_PUBLIC, ?int $position = null, bool $passByReference = false): PromotedParameterGenerator
+    {
+        $parameter = new PromotedParameterGenerator($name, $type, $visibility, $position, $passByReference);
 
         $this->setParameter($parameter);
 
