2961a9f18f0049b6eee48f7a0785a204d9c4e3c2
William Knauss <william.knauss@kingdom.com>
2023-02-20 03:09:22 -0500

    u2d

---
+++ src/ClassGenerator.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FightTheIce\Code;
+
+use Laminas\Code\Generator\ClassGenerator as Laminas_ClassGenerator;
+use Laminas\Code\Generator\TypeGenerator;
+
+class ClassGenerator extends Laminas_ClassGenerator
+{
+    public function newDocBlockGenerator(mixed $shortDescription = null, mixed $longDescription = null, array $tags = []): DocBlockGenerator
+    {
+        $docblock = new DocBlockGenerator($shortDescription, $longDescription, $tags);
+
+        $this->setDocBlock($docblock);
+
+        return $docblock;
+    }
+
+    public function newMethodGenerator(mixed $name = null, array $parameters = [], int $flags = MethodGenerator::FLAG_PUBLIC, mixed $body = null, mixed $docBlock = null): MethodGenerator
+    {
+        $method = new MethodGenerator($name, $parameters, $flags, $body, $docBlock);
+
+        $this->addMethodFromGenerator($method);
+
+        return $method;
+    }
+
+    public function newPropertyGenerator(?string $name = null, mixed $defaultValue = null, int $flags = PropertyGenerator::FLAG_PUBLIC, ?TypeGenerator $type = null): PropertyGenerator
+    {
+        $property = new PropertyGenerator($name, $defaultValue, $flags, $type);
+
+        $this->addPropertyFromGenerator($property);
+
+        return $property;
+    }
+}
---
+++ src/ParameterGenerator.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FightTheIce\Code;
+
+use Laminas\Code\Generator\ParameterGenerator as Laminas_ParameterGenerator;
+use Laminas\Code\Generator\PropertyValueGenerator;
+
+class ParameterGenerator extends Laminas_ParameterGenerator {
+    public function newPropertyValueGenerator($value = null, $type = PropertyValueGenerator::TYPE_AUTO, $outputMode = PropertyValueGenerator::OUTPUT_MULTIPLE_LINE, $constants = null): PropertyValueGenerator {
+        $value = new PropertyValueGenerator($value,$type,$outputMode,$constants);
+
+        $this->setDefaultValue($value);
+
+        return $value;
+    }
+}
