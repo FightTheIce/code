baa120bab8b2a61d4ceccf9ece0809ba1a680062
William Knauss <william.knauss@kingdom.com>
2023-02-20 23:34:03 -0500

    u2d

--- src/MethodGenerator.php
+++ src/MethodGenerator.php
@@ -8,6 +8,7 @@ use Exception;
 use Laminas\Code\Generator\MethodGenerator as Laminas_MethodGenerator;
 use Laminas\Code\Generator\ParameterGenerator;
 use Laminas\Code\Generator\PromotedParameterGenerator;
+use Laminas\Code\Reflection\ClassReflection;
 
 class MethodGenerator extends Laminas_MethodGenerator
 {
@@ -86,5 +87,30 @@ class MethodGenerator extends Laminas_MethodGenerator
         $this->setParameter($parameter);
 
         return $parameter;
+    }
+
+    public function importSource(string $class, ?string $method = null): self
+    {
+        if (is_null($method)) {
+            $method = $this->getName();
+        }
+
+        /**
+         * @psalm-suppress ArgumentTypeCoercion
+         */
+        $reflect = new ClassReflection($class); //@phpstan-ignore-line
+
+        if ($reflect->hasMethod($method) === false) {
+            throw new Exception(
+                'Class '.$class.' does not have method: '.$method.'!'
+            );
+        }
+
+        $method = $reflect->getMethod($method);
+
+        //DODO - Fix indendation problems
+        $this->setBody($method->getBody());
+
+        return $this;
     }
 }
